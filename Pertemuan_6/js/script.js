18e5876b98efa97a19a778f8943140f0f85176f1
Arifful <ariffulfikri.af@gmail.com>
2024-10-30 13:42:33 +0700

    pertemuan 6

---
+++ Pertemuan_6/js/script.js
@@ -0,0 +1,26 @@
+console.log("Hello from");
+
+// let message = "Hello my name is john";
+// alert(message);
+
+// let test = prompt("Username")
+
+// let isok = confirm("are you sure?");
+// alert(isok);
+
+// string
+let message = "my name is john";
+//int
+let age = 20;
+//float
+let pi = 10.5;
+// Boolean
+let islogin = true
+// Object
+let user = {
+    name : "john",
+    age : 25,
+    islogin : True
+}
+// Array
+let hobbies = ['coding', 'music']
