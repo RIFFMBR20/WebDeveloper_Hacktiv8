a148dd39c2f90749ff9f646444d3773ee9215bbe
Arifful <ariffulfikri.af@gmail.com>
2024-10-31 09:02:50 +0700

    Pertemuan 7

---
+++ Pribadi/index.js
@@ -0,0 +1,79 @@
+// let name = 'Mosh';
+// console.log(name);
+
+// let firstName = "Arifful"
+// let lastName = "Fikri"
+// console.log(firstName, lastName)
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// const interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+// let name = "mosh";
+// let age = 30;
+// let isaApproved = true;
+// let firstName = undefined;
+// let selectedColor = null;
+
+// let name = "mosh";
+// let age = 30;
+// let person = {
+//     name : 'Mosh',
+//     age : 30,
+// };
+
+// person.age = 100;
+// person.name = "john";
+
+// let selection = 'name'
+// person[selection] = "Mary";
+
+// console.log(person)
+
+// let selectedColor = ['red', 'blue'];
+// selectedColor[2] = 'green'
+// console.log(selectedColor.length);
+
+// function greet(name) {
+//     console.log("Hello " + name)
+// };
+
+// greet("john");
+
+// const userName = "john";
+// const welcomeMsg = document.getElementById('hello-msg')
+
+// welcomeMsg.textContent += userName === " " ? 'Guest' : userName;
+
+document.title = "Fikri Arifful"
+// const body = document.body
+// body.append("Hello World")
+
+// // mengisi
+// const h1 = document.createElement('h1')
+// h1.textContent = "Anjay"
+// body.append(h1)
+
+// const namaSaya = document.createElement('p')
+// namaSaya.innerHTML = "Arifful"
+// body.append(namaSaya)
+
+// const namaKamu = document.createElement('b')
+// namaKamu.innerText = "Fikri"
+// body.append(namaKamu)
+
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+
+console.log(btn1)
+console.log(btn2)
+
+btn1.style.border = 'none'
+btn1.style.padding = '8px'
+btn1.style.fontSize = '24px'
+btn1.style.background = 'tomato'
+
